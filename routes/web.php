81d819c90a7be57ebcb2d06acbc55a681527d3f2
nxxxiu <1623248430@qq.com>
2019-07-16 08:13:40 +0000

    日志

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,13 @@ Route::get('/', function () {
 
 Route::get('/index','OssController@index');
 Route::get('/videoindex','OssController@videoindex');
+Route::get('/notify','OssController@notify');//OSS事件通知
 
 //转移本地文件
 Route::get('/saveToOss','VideoController@saveToOss');
 
 //视频详情页
 Route::get('/detail','VideoController@detail');
+
+
 Route::get('/aliyun','VideoController@aliyun');
